000e829d78d0cf748857cbb37019c8ae6faa38ab
devchia254 <alex7chiaster@gmail.com>
2019-11-24 23:37:36 +0800

    React done, implementing RoR DB

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react'
+import Jumbotron from './Jumbotron'
+import Table from './Table/Table'
 
 class Home extends Component {
     constructor(){
         super()
         this.state = {
             course_modules: [
-                {id: 1, title: '1. Setting up a new Ruby on Rails App with React', description: 'lorem ipsum', active: false},
-                {id: 2, title: '2. Adding React to an existing Rails App', description: 'lorem ipsum', active: false},
-                {id: 3, title: '3. Building a Hello World App', description: 'lorem ipsum', active: false},
-                {id: 4, title: '4. Adding Router DOM to your App', description: 'lorem ipsum', active: false}
+              {id: 1, title: '1. Setting up a new Ruby on Rails App with React', description: 'lorem ipsum', active: false},
+              {id: 2, title: '2. Adding React to an existing Rails App', description: 'lorem ipsum', active: true},
+              {id: 3, title: '3. Building a Hello World App', description: 'lorem ipsum', active: false},
+              {id: 4, title: '4. Adding Router DOM to your App', description: 'lorem ipsum', active: false}
             ]
         }
     }
 
+    handleVideoChange(item, event) {
+        event.preventDefault()
+        
+        let course_modules = [...this.state.course_modules];
+
+        course_modules.map(data => {
+            data.active = false;
+        })
+
+        item.active = !item.active;
+
+        course_modules[item.id - 1] = item;
+        
+        this.setState({course_modules});
+    }
+
     render(){
         return(
-            <div>This is our Home Component</div>
+            <div>
+                <Jumbotron />
+                <Table handleVideoChange={this.handleVideoChange.bind(this)} course_modules={this.state.course_modules}/>
+            </div>
         )
     }
 }
